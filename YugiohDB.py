b804005b55bd984bffc49acd2d3a2f8e840d99de
huynhchin <chuynh@zagmail.gonzaga.edu>
2017-12-10 13:27:22 -0800

    option 2

--- YugiohDB.py
+++ YugiohDB.py
@@ -1,40 +1,53 @@
 import mysql.connector
 import config
 
+try:
+    usr = config.mysql['user']
+    pwd = config.mysql['password']
+    hst = config.mysql['host']
+    db = 'chuynh_DB'
+    # create a connection
+    con = mysql.connector.connect(user=usr, password=pwd, host=hst, database=db)
+except mysql.connector.Error as err:
+        print(err)
+
 #the user creates his own deck
 def createDeck():
-    name = input("Enter name for deck: ")
+    dName = input("Enter name for deck: ")
+    description = input("Enter a description for this deck: ")
+    rs = con.cursor()
+    query = 'INSERT INTO Deck VALUES (%s, %s)'
+    rs.execute(query, (dName, description))
+    con.commit()
+    print("")
+    print("Deck has been added.")
+    rs.close()
 
 #Main Menu
 def main():
-    try:
-        usr = config.mysql['user']
-        pwd = config.mysql['password']
-        hst = config.mysql['host']
-        db = 'chuynh_DB'
-        # create a connection
-        con = mysql.connector.connect(user=usr, password=pwd, host=hst, database=db)
-        exitProgram = False
-        while exitProgram == False:
-            print("")
-            print("1. Look at existing")
-            print("2. Add country")
-            print("3. Find countries based on gdp and inflation")
-            print("4. Update country's gdp and inflation")
-            print("5. Exit")
-            command = input("Enter your choice (1-5): ")
-            print("")
-            if command == 1:
-            elif command == 2:
-            elif command == 3:
-            elif command == 4:
-            elif command == 5:
-                exitProgram = True
-                con.close()
-            else:
-                print("Not a valid selection, please pick from the choices above.")
-    except mysql.connector.Error as err:
-        print(err)
+    exitProgram = False
+    while exitProgram == False:
+        print("")
+        print("1. Look at existing deck.")
+        print("2. Create your own deck.")
+        print("3. Add card to a deck.")
+        print("4. Look at specific card")
+        print("5. Exit")
+        command = input("Enter your choice (1-5): ")
+        print("")
+        if command == 1:
+            print("1")
+        elif command == 2:
+            createDeck()
+        elif command == 3:
+            print("3")
+        elif command == 4:
+            print("4")
+        elif command == 5:
+            con.close()
+            exitProgram = True
+        else:
+            print("Not a valid selection, please pick from the choices above.")
 
 if __name__ == '__main__':
     main()
